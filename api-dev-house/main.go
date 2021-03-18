8873f773f318892c4f56b23dc5c0675039036f44
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-18 13:00:27 -0300

    build: adicionado pacote de conf e criado configuracao da aplicao com .env

--- api-dev-house/main.go
+++ api-dev-house/main.go
@@ -6,11 +6,18 @@ import (
 	"net/http"
 
 	"api-dev-house/src/router"
+
+	"api-dev-house/src/config"
 )
 
 func main() {
+
+	config.Load()
+
 	fmt.Println("connect to api-dev-house")
+	fmt.Printf("Listen on localhost: %d", config.Port)
 
 	router := router.GenerateRouter()
-	log.Fatal(http.ListenAndServe(":5000", router))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), router))
+
 }
