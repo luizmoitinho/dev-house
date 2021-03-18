017dee4079be2c677563121a7007b3f73e05835a
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-18 13:25:52 -0300

    feat: criacao do metodo insert user

--- api-dev-house/src/repository/userRepository.go
+++ api-dev-house/src/repository/userRepository.go
@@ -16,7 +16,24 @@ func NewRepositoryUser(db *sql.DB) *Users {
 }
 
 //Insert ... persiste um novo usuario
-func (u Users) Insert(usuario models.User) (int64, error) {
+func (u Users) Insert(user models.User) (int64, error) {
+	stm, err := u.db.Prepare("INSERT INTO tb_users (name, login, email, password) 	VALUES (?,?,?,?)")
 
-	return 0, nil
+	if err != nil {
+		return 0, err
+	}
+	defer stm.Close()
+
+	res, err := stm.Exec(user.Name, user.Login, user.Email, user.Password)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastInsertId, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return lastInsertId, nil
 }
