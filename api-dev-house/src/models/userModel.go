5ff1ae57ff1af35f2e5f7e3a1b751eedc9b4e06c
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-03-23 12:49:33 -0300

    ref: adicionando validaçoes no cadastro de usuario

--- api-dev-house/src/models/userModel.go
+++ api-dev-house/src/models/userModel.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //User .., dados de um usuario
 type User struct {
@@ -10,4 +14,38 @@ type User struct {
 	Email     string    `json:"email,omitempty"`
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+//Prepare ... chama métodos para validar e formatar dados do usuário a ser inserido.
+func (u *User) Prepare() error {
+	if err := u.validate(); err != nil {
+		return err
+	}
+	u.format()
+	return nil
+}
+
+func (u *User) validate() error {
+
+	if u.Name == "" {
+		return errors.New("Campo Nome é obrigatório")
+	}
+	if u.Login == "" {
+		return errors.New("Campo Login é obrigatório")
+	}
+	if u.Email == "" {
+		return errors.New("Campo Email é obrigatório")
+	}
+	if u.Password == "" {
+		return errors.New("Campo Senha é obrigatório")
+	}
+
+	return nil
+}
+
+func (u *User) format() {
+	u.Name = strings.TrimSpace(u.Name)
+	u.Login = strings.TrimSpace(u.Login)
+	u.Email = strings.TrimSpace(u.Email)
+
 }
