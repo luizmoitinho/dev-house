d85e092ea186e1d50dcf5796b992dfb7a1715d6e
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-04-01 23:35:49 -0300

    ci: script com tabela de followers

--- api-dev-house/sql/script.sql
+++ api-dev-house/sql/script.sql
@@ -3,6 +3,8 @@ CREATE DATABASE IF NOT EXISTS devhouse;
 USE devhouse;
 
 DROP TABLE IF EXISTS tb_users;
+DROP TABLE IF EXISTS tb_followers;
+
 
 CREATE TABLE tb_users(
   user_id INT auto_increment PRIMARY KEY,
@@ -11,4 +13,19 @@ CREATE TABLE tb_users(
   email VARCHAR(100) NOT NULL UNIQUE,
   password TEXT not null,
   created_at timestamp default now()
-)ENGINE=INNODB 
+)ENGINE=INNODB; 
+
+CREATE TABLE tb_followers(
+ user_id INT NOT NULL, 
+ follower_id INT NOT NULL
+)ENGINE=INNODB;
+
+ALTER TABLE tb_followers ADD CONSTRAINT FK_followers_users_1
+  FOREIGN KEY (user_id) REFERENCES tb_users(user_id) ON DELETE CASCADE;
+
+
+ALTER TABLE tb_followers ADD CONSTRAINT FK_followers_users_2
+  FOREIGN KEY (follower_id) REFERENCES tb_users(user_id) ON DELETE CASCADE;
+
+ALTER TABLE tb_followers ADD CONSTRAINT PK_followers
+  PRIMARY KEY(user_id, follower_id);
