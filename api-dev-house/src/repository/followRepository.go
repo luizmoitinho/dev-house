3cf35c38e02659ad228ca1dbffaff5d477d18f4b
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-04-04 00:16:07 -0300

    feat: metodo de repository para dar unfollow

--- api-dev-house/src/repository/followRepository.go
+++ api-dev-house/src/repository/followRepository.go
@@ -14,6 +14,7 @@ func NewRepositoryFollow(db *sql.DB) *Follow {
 	return &Follow{db}
 }
 
+//Follow ... faz com que um usuário siga outro usuario
 func (f *Follow) Follow(userID, followingID int64) error {
 	stm, err := f.db.Prepare("INSERT INTO tb_followers (user_id, following_id) VALUES (?,?)")
 	if err != nil {
@@ -28,6 +29,23 @@ func (f *Follow) Follow(userID, followingID int64) error {
 	return nil
 }
 
+//UnFollow ... para de seguir um determinando usuário
+func (f *Follow) UnFollow(userID, followingID int64) error {
+	stm, err := f.db.Prepare("DELETE FROM tb_followers WHERE user_id = ? AND following_id = ?")
+
+	if err != nil {
+		return nil
+	}
+	defer stm.Close()
+
+	if _, err := stm.Exec(userID, followingID); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+//IsFollow ... verifica se um usuário já segue um outro usuario
 func (f *Follow) IsFollow(userID, followingID int64) (bool, error) {
 	query, err := f.db.Query("SELECT true FROM tb_followers WHERE user_id = ? AND following_id = ? ", userID, followingID)
 	if err != nil {
