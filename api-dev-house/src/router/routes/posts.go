eb23a0e1f8b1c9c1e54951273fa32d129d1d1385
luizmoitinho <luizcarlos.moitinho@medlynx.com.br>
2021-04-19 08:58:08 -0300

    feat: adicao das rotas de posts

---
+++ api-dev-house/src/router/routes/posts.go
@@ -0,0 +1,36 @@
+package routes
+
+import "net/http"
+
+var PostsRoutes = []Route{
+	{
+		URI:           "/posts",
+		Method:        http.MethodPost,
+		MethodRequest: controllers.CreatePost,
+		Authorization: true,
+	},
+	{
+		URI:           "/posts",
+		Method:        http.MethodGet,
+		MethodRequest: controllers.GetPosts,
+		Authorization: true,
+	},
+	{
+		URI:           "/posts/{id}",
+		Method:        http.MethodGet,
+		MethodRequest: controllers.GetPost,
+		Authorization: true,
+	},
+	{
+		URI:           "/posts/{id}",
+		Method:        http.MethodPut,
+		MethodRequest: controllers.UpdatePost,
+		Authorization: true,
+	},
+	{
+		URI:           "/posts/{id}",
+		Method:        http.MethodDelete,
+		MethodRequest: controllers.DeletePost,
+		Authorization: true,
+	},
+}
